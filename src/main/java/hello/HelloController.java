369edfc79ebf54825be2fa998c0fd91a468da49e
RedVirtual <42968928+RedVirtual@users.noreply.github.com>
2018-09-04 17:54:25 +0200

    changes in resources

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,14 +1,18 @@
 package hello;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
-    }
+	@Value("${introduccion.spring.nombre}")
+	private String nombre;
+
+	@RequestMapping("/")
+	public String index() {
+		return  nombre; //"Greetings from Spring Boot!";
+	}
 
 }
